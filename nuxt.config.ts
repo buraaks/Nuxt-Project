9098e4624de3162c4b7ead1c9747630eed39d0ca
buraaks <buraktemur0816@gmail.com>
2026-02-10 15:17:46 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  ssr: false,
-
+  
+  // Statik hosting (FTP) için en önemli ikili:
+  ssr: false, 
+  nitro: {
+    preset: 'static'
+  },
 
   app: {
     head: {
+      title: 'Nuxt Projesi',
+      htmlAttrs: {
+        lang: 'tr'
+      },
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
@@ -17,7 +25,8 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' }
       ],
       meta: [
-        { name: 'theme-color', content: '#ffffff' }
+        { name: 'theme-color', content: '#ffffff' },
+        { name: 'description', content: 'Nuxt 4 ile geliştirilmiş statik web projesi' }
       ]
     }
   }
